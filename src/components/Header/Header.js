0711e9b86d973ed6b14f5974070590d969681cf8
Habidhossen <habidhossen2@gmail.com>
2022-03-23 17:56:19 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "../../images/Logo.svg"; //
+import "./Header.css";
 
 const Header = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="" />
+      <div>
+        <a href="">Order</a>
+        <a href="">Order Review</a>
+        <a href="">Manage Inventory</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
